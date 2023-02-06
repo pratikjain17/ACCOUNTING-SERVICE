661f698aba1b5087885abb04b8e0f27ea8a537cb
Pratik <pratik.pj48@gmail.com>
2023-02-06 15:00:50 +0530

    change consumer method

--- src/main/java/com/pratik/www/salary/service/AccountingConsumer.java
+++ src/main/java/com/pratik/www/salary/service/AccountingConsumer.java
@@ -3,37 +3,54 @@ package com.pratik.www.salary.service;
 import com.pratik.www.salary.db.entity.SalaryEntity;
 import com.pratik.www.salary.model.Employee;
 import com.pratik.www.salary.model.EmployeeLeave;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.protocol.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class AccountingConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountingConsumer.class);
 
-    @KafkaListener(topics = "${spring.kafka.topic1}, ${spring.kafka.topic2}",
+//    @Autowired
+//    Environment env;
+//
+//    private String topic1 = env.getProperty("spring.kafka.topic1");
+//    private String topic2 = env.getProperty("spring.kafka.topic2");
+
+    @KafkaListener(topics={"${spring.kafka.topic1}"} ,
             groupId = "${spring.kafka.consumer.group-id}")
-    public SalaryEntity consumeAndCalculate(Employee employee, EmployeeLeave employeeLeave){
-        SalaryEntity salary;
-        Number amount = null;
-
-        Number baseSalary = employee.getBaseSalary();
-        if(employeeLeave == null){
-            amount = baseSalary;
-        }else {
-            Number leaveCount = employeeLeave.getCount();
-            Number daysInMonth = calculateDaysInMonth(employeeLeave.getYearMonth());
-            amount = (baseSalary.intValue()) * (daysInMonth.intValue() - leaveCount.intValue()) / daysInMonth.intValue();
-        }
-
-        salary = new SalaryEntity.SalaryEntityBuilder()
-                .setEmployeeId(employee.getId())
-                .setYearMonth(employeeLeave.getYearMonth())
-                .setAmount(amount)
-                .build();
-
-        return salary;
+    public void consumeAndCalculate(ConsumerRecord<String, Employee> employeeConsumerRecord){
+        LOGGER.info(String.format("employee_topics Event consumed:  => %s", employeeConsumerRecord.value().toString()));
+//        LOGGER.info(String.format("workhour_topics Event consumed:  => %s", employeeLeave.getPayload().toString()));
+//        SalaryEntity salary;
+//        Number amount = null;
+//
+//        Number baseSalary = employee.getPayload().getBaseSalary();
+//        if(employeeLeave == null){
+//            amount = baseSalary;
+//        }else {
+//            Number leaveCount = employeeLeave.getPayload().getCount();
+//            Number daysInMonth = calculateDaysInMonth(employeeLeave.getPayload().getYearMonth());
+//            amount = (baseSalary.intValue()) * (daysInMonth.intValue() - leaveCount.intValue()) / daysInMonth.intValue();
+//        }
+//
+//        salary = new SalaryEntity.SalaryEntityBuilder()
+//                .setEmployeeId(employee.getPayload().getId())
+//                .setYearMonth(employeeLeave.getPayload().getYearMonth())
+//                .setAmount(amount)
+//                .build();
+//
+//        LOGGER.info(String.format("Salary calculated: => %s", salary.toString()));
+//        return salary;
     }
 
     private Number calculateDaysInMonth(Number yearMonth) {
